d314484fa185ea1ad1d80c445976386fb0929795
akinlope <akinrimisitt@gmail.com>
2022-11-30 14:08:18 +0100

    tast 2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
+# number = random.randint(-10000, 10000)
+number = -626
 numStr = repr(number)
 lastDigitStr = numStr[-1]
 lastNumber = int(lastDigitStr)
+if number > 0:
+    if lastNumber > 5:
+        print("Last digit of", number, "is", lastNumber, "and is greater than 5")
+    elif lastNumber == 0:
+        print("Last digit of", number, "is", lastNumber, "and is 0")
+    elif (lastNumber < 6) and (lastNumber != 0):
+        print("Last digit of", number, "is", "-"+str(lastNumber), "and is less than 6 and not 0")
 
-if lastNumber > 5:
-    print("Last digit of", number, "is", lastNumber, "and is greater than 5")
-elif lastNumber == 0:
-    print("Last digit of", number, "is", lastNumber, "and is 0")
-elif (lastNumber < 6) and (lastNumber != 0):
-    print("Last digit of", number, "is", lastNumber, "and is less than 6 and not 0")
+elif number < 0:
+    # if lastNumber == 0:
+    #     print("Last digit of", number, "is", lastNumber, "and is 0")
+    if (lastNumber < 6) and (lastNumber != 0):
+        print("Last digit of", "-"+number, "is", "-"+str(lastNumber), "and is less than 6 and not 0") 
 # print(number)
 # print(lastNumber)
---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+n = 26
+res = " "
+for letters in range(97,97 + n):
+    res = res + chr(letters)
+
+print(res)
