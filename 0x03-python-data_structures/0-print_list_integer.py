2cf422f6c2e94fb3dac31a5bb1b9f3e793faca57
akinlope <akinrimisitt@gmail.com>
2022-12-05 14:31:39 +0100

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,5 @@
 def print_list_integer(my_list=[]):
     getLen = len(my_list)
     for i in range(getLen):
-        print(my_list[i])
-        print(type(my_list[i]))
+        print("{:d}".format(my_list[i]))
+        # print(type(my_list[i]))
