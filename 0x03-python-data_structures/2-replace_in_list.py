a2fa777a89747ed36468ef36063f903a3b10a9b2
akinlope <akinrimisitt@gmail.com>
2022-12-05 15:15:26 +0100

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    myLen = len(my_list)
+    myLen = len(my_list) - 1
     if idx < 0 or idx > myLen:
         return (my_list)
     else:
