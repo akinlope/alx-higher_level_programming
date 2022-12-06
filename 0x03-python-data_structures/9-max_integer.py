1fc652a320705877d1c8fee1eca644753b900a30
akinlope <akinrimisitt@gmail.com>
2022-12-06 23:11:36 +0100

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     firstIndex = my_list[0]
-    arrList = len(my_list)
+    arrList = len(my_list) - 1
     # print(arrList)
     if arrList == 0:
         return(None)
