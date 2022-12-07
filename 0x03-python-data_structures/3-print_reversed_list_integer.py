d1c05c131b6d1119c763130a52d1757db9859e28
akinlope <akinrimisitt@gmail.com>
2022-12-07 12:19:51 +0100

    task 3 fixed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,8 @@
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
         pass
-    ownList = my_list.copy()
-    ownList.reverse()
-    for i in ownList:
-        print("{}".format(i))
+    else:
+        ownList = my_list.copy()
+        ownList.reverse()
+        for i in ownList:
+            print("{}".format(i))
