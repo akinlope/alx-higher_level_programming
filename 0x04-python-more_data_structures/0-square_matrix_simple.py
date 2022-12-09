b4b64ce6928131cd254b01d0047bd5cc8a17aaee
akinlope <akinrimisitt@gmail.com>
2022-12-09 12:02:01 +0100

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    myArr = matrix.copy()
+    myLen = range(len(myArr))
+
+    for i in myLen:
+        myArr[i] = list(map(lambda x: x * x, myArr[i]))
+    return(myArr)
+
+
