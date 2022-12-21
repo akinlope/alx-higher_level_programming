e9df2cf437b9ce5266d03dd2d420ee319fdd7657
akinlope <akinrimisitt@gmail.com>
2022-12-21 18:43:29 +0100

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a class Square"""
+
+
+class Square:
+
+    def __init__(self, size = 0):
+        """ a class Square
+        create a new Instance of the Square
+        Args:
+            size (int): size of new Square
+        """
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif(size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
