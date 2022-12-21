6059a37e7bb14251e6564412917d5f2bf701d7c3
akinlope <akinrimisitt@gmail.com>
2022-12-21 18:24:44 +0100

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a class Square"""
+class Square:
+    def __init__(self, size):
+        """ a class Square
+        create a new Instance of the Square
+        Args:
+            size (int): size of new Square
+        """
+        self.__size = size
